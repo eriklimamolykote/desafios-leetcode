784e103b7f7e28527e558bb56f617ab51d23e995
eriklimamolykote <eriklim@hotmail.com>
2022-04-26 11:12:07 -0300

    commit-desafios-leetcode-26-04-2022

---
+++ leetcode_javascript/Leet22.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthGrammar = function(n, k) {
+
+    if ( n == 1 ) return 0;
+
+    if ( k % 2 == 0 ) return kthGrammar( n - 1, k / 2 ) == 0 ? 1 : 0;
+
+    return kthGrammar( n - 1, ( k + 1 ) / 2 );
+
+};
